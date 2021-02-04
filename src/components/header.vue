d6248b6f52af4f67e0459674d41cd7c2e9f2e35a
ajzimm <47437378+ajzimm@users.noreply.github.com>
2021-02-03 21:15:44 -0500

    Initial setup

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,19 +1,16 @@
 <template>
   <span class="typcn-social-youtube"></span>
-  <header></header>
-  <p class="text-xl text-red-900">Hello</p>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: 'header',
   components: {}
 }
 </script>
 
 <style>
-#app {
+#header {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
